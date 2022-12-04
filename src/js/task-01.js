d53e119f2693f03dca3fc2a82c7b8424c4a6a4b7
HelenLebed <110856304+HelenLebed@users.noreply.github.com>
2022-12-04 19:34:40 +0200

    1 task

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const listElement = document.querySelector("#categories");
+
+const children = listElement.children;
+
+console.log("Number of categories:", children.length);
+
+for (const liItem of children) {
+    printCategory(liItem);
+}
+
+function printCategory(item) {
+    const itemChildren = item.children;
+    console.log("");
+
+    const title = itemChildren[0].textContent;
+    console.log("Category:", title);
+    const listItems = itemChildren[1].children.length;
+    console.log("Elements:", listItems);
+}
