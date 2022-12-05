6ef014661e829cdd097f7d4b3088f61276bcd848
HelenLebed <110856304+HelenLebed@users.noreply.github.com>
2022-12-05 22:12:50 +0200

    task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,22 @@
+let counterValue = 0;
+const count = document.querySelector("#value");
+
+const buttonDec = document.querySelector('button[data-action="decrement"]');
+const buttonInc = document.querySelector('button[data-action="increment"]');
+
+// console.dir(buttonDec);
+// console.dir(buttonInc);
+
+const handleClick = (event) => {
+    counterValue -= 1;
+    count.textContent = counterValue;
+};
+
+buttonDec.addEventListener("click", handleClick);
+
+const handleClick2 = (event) => {
+    counterValue += 1;
+    count.textContent = counterValue;
+};
+
+buttonInc.addEventListener("click", handleClick2);
